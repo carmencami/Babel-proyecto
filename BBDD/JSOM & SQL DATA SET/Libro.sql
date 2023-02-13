ec5f4fc4facb4042dbd36dc7034eb19722ebc4f8
Juan Pedro Mancera Taboada <55956200+juan2pedro@users.noreply.github.com>
2023-02-13 13:45:36 +0100

    Añadir archivos tanto JSON como SQL con DATA SET
    Hace falta añadir la tabla IMG y cambiar la dirección de todas las imágenes ya que actualmente tiene datos aleatorios.

---
+++ BBDD/JSOM & SQL DATA SET/Libro.sql
@@ -0,0 +1,60 @@
+INSERT INTO Libro (ID_Libro,Nombre,Imagen,Resumen,ISBN)
+VALUES
+  (1,'Lila B. Lozano','/en-ca','est. Nunc ullamcorper, velit in aliquet lobortis, nisi nibh lacinia orci, consectetuer euismod est arcu ac orci. Ut',4486917021628),
+  (2,'Xena B. Carmona','/en-us','justo. Proin non massa non ante bibendum ullamcorper. Duis cursus, diam at pretium aliquet, metus urna convallis erat, eget tincidunt',9803275090008),
+  (3,'Tanner H. Redondo','/user/110','commodo tincidunt nibh. Phasellus nulla. Integer vulputate, risus a ultricies adipiscing,',2951089738008),
+  (4,'Lael V. Velasco','/sub','dolor quam, elementum at, egestas a, scelerisque sed, sapien. Nunc pulvinar arcu et pede. Nunc sed orci lobortis augue scelerisque',7315402014497),
+  (5,'Zachary Z. Moreno','/fr','tellus. Suspendisse sed dolor. Fusce mi',3776169661748),
+  (6,'Odysseus Mendez','/site','sed, facilisis vitae, orci. Phasellus dapibus quam quis diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',1887003482262),
+  (7,'Nicholas L. Ramos','/settings','enim consequat purus. Maecenas libero est, congue a, aliquet vel, vulputate eu, odio. Phasellus at augue id ante dictum cursus. Nunc',9145291373048),
+  (8,'Emmanuel C. Iglesias','/settings','quis massa. Mauris vestibulum, neque sed dictum eleifend, nunc risus varius orci, in consequat enim diam vel',6389891742162),
+  (9,'Kennan U. Diez','/en-us','pede sagittis augue, eu tempor erat',8749372315980),
+  (10,'Yvonne Herrera','/settings','vestibulum lorem, sit amet ultricies sem magna nec quam.',3211874959728);
+INSERT INTO Libro (ID_Libro,Nombre,Imagen,Resumen,ISBN)
+VALUES
+  (11,'Amal U. Leon','/group/9','rutrum non, hendrerit id, ante. Nunc mauris sapien, cursus in,',5586957300744),
+  (12,'Jermaine R. Rubio','/en-ca','Sed nunc est, mollis non, cursus non, egestas a, dui. Cras pellentesque. Sed dictum. Proin eget odio. Aliquam vulputate',6408328103134),
+  (13,'Liberty Muﾑoz','/sub','feugiat metus sit amet ante. Vivamus non lorem vitae odio',7316956697333),
+  (14,'Sawyer Suarez','/one','at pretium aliquet, metus urna convallis erat, eget tincidunt dui augue eu tellus. Phasellus',9699141041182),
+  (15,'Tamara M. Gomez','/en-us','tempus, lorem fringilla ornare placerat, orci lacus vestibulum lorem, sit amet ultricies sem magna nec',9245534140483),
+  (16,'Bell B. Miguel','/settings','non, dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero. Proin sed turpis nec mauris blandit mattis. Cras eget',3227709804550),
+  (17,'Jordan W. Diez','/en-ca','lectus sit amet luctus vulputate, nisi sem',1718267632016),
+  (18,'Rigel Gimenez','/sub/cars','sit amet diam eu dolor egestas rhoncus. Proin nisl sem,',8080910465792),
+  (19,'Thaddeus Diaz','/fr','et magnis dis parturient montes,',5391320795296),
+  (20,'Donovan N. Miguel','/sub/cars','lacus, varius et, euismod et, commodo at, libero.',4040825001165);
+INSERT INTO Libro (ID_Libro,Nombre,Imagen,Resumen,ISBN)
+VALUES
+  (21,'Lance Blanco','/en-ca','urna. Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla',7538854373131),
+  (22,'Hillary S. Gonzalez','/site','lorem eu metus. In lorem. Donec elementum, lorem ut aliquam iaculis, lacus pede sagittis augue, eu',6813619705002),
+  (23,'Carla Gutierrez','/site','Suspendisse eleifend. Cras sed leo. Cras vehicula aliquet libero. Integer in magna. Phasellus dolor elit, pellentesque a,',1964767267682),
+  (24,'Chantale Alvarez','/user/110','dignissim. Maecenas ornare egestas ligula. Nullam feugiat placerat velit. Quisque',4201615542524),
+  (25,'Nehru Calvo','/sub/cars','Nullam nisl. Maecenas malesuada fringilla est. Mauris eu turpis. Nulla aliquet. Proin velit. Sed malesuada augue ut lacus. Nulla tincidunt, neque vitae semper',9992864125188),
+  (26,'Elizabeth Garrido','/site','ut, nulla. Cras eu tellus eu augue porttitor interdum. Sed auctor odio a purus. Duis elementum, dui quis accumsan convallis, ante lectus',5479824125379),
+  (27,'Cally H. Romero','/site','Mauris molestie pharetra nibh. Aliquam ornare, libero at auctor ullamcorper, nisl arcu iaculis enim, sit amet ornare lectus justo eu',3301530137073),
+  (28,'Sydney Miguel','/sub/cars','per conubia nostra, per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare. In faucibus. Morbi vehicula. Pellentesque tincidunt',5722404875852),
+  (29,'Jermaine A. Mendez','/site','ipsum dolor sit amet, consectetuer adipiscing elit. Etiam laoreet, libero et tristique',4487585338031),
+  (30,'Jakeem Cortes','/en-us','dignissim. Maecenas ornare egestas ligula. Nullam feugiat placerat velit. Quisque varius. Nam porttitor scelerisque neque.',9631208912222);
+INSERT INTO Libro (ID_Libro,Nombre,Imagen,Resumen,ISBN)
+VALUES
+  (31,'Kim E. Muñoz','/fr','dui. Suspendisse ac metus vitae velit',1761245226305),
+  (32,'Erin Esteban','/sub/cars','eleifend nec, malesuada ut, sem. Nulla interdum. Curabitur dictum. Phasellus in felis. Nulla tempor augue ac ipsum. Phasellus vitae',8644958333530),
+  (33,'Flynn R. Gomez','/fr','lorem ipsum sodales purus, in molestie tortor nibh',6525867588373),
+  (34,'Raven Bravo','/one','orci lobortis augue scelerisque mollis. Phasellus libero mauris, aliquam eu, accumsan sed, facilisis vitae, orci. Phasellus dapibus quam quis diam.',6363985092484),
+  (35,'Sylvester L. Medina','/fr','dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus. Nulla eget metus eu erat semper rutrum. Fusce dolor',8658137676978),
+  (36,'Patrick X. Nuñez','/one','enim. Sed nulla ante, iaculis nec, eleifend non, dapibus rutrum,',2243545777937),
+  (37,'Dante H. Flores','/sub/cars','a odio semper cursus. Integer mollis.',4678108027781),
+  (38,'Asher B. Marquez','/settings','parturient montes, nascetur ridiculus mus. Proin vel arcu eu odio tristique pharetra. Quisque ac libero nec ligula consectetuer rhoncus. Nullam velit dui, semper et,',7377429481899),
+  (39,'Chelsea O. Pascual','/site','mus. Proin vel arcu eu odio tristique pharetra. Quisque ac',9652462168067),
+  (40,'Hedley Izquierdo','/en-us','Cras sed leo. Cras vehicula aliquet libero. Integer in magna. Phasellus dolor elit, pellentesque a, facilisis',9393134311676);
+INSERT INTO Libro (ID_Libro,Nombre,Imagen,Resumen,ISBN)
+VALUES
+  (41,'Ginger Torres','/one','eu lacus. Quisque imperdiet, erat nonummy ultricies ornare, elit elit fermentum risus, at fringilla purus mauris a',5216739894917),
+  (42,'Jonah O. Gutierrez','/settings','ut erat. Sed nunc est, mollis non, cursus non, egestas a, dui.',4037452062788),
+  (43,'Fatima X. Mora','/en-us','semper tellus id nunc interdum feugiat. Sed nec metus facilisis lorem tristique aliquet. Phasellus fermentum convallis ligula.',3393075084359),
+  (44,'Willow Perez','/sub','Fusce aliquet magna a neque. Nullam ut nisi a odio semper cursus. Integer mollis. Integer tincidunt aliquam arcu. Aliquam ultrices iaculis odio. Nam interdum',9209267285012),
+  (45,'Ian T. Carrasco','/one','aliquet. Phasellus fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget, venenatis a, magna. Lorem ipsum dolor sit amet,',5996746636986),
+  (46,'Fatima I. Fernandez','/en-us','Proin vel nisl. Quisque fringilla euismod',8868801042829),
+  (47,'Chanda Q. Pastor','/sub/cars','dolor sit amet, consectetuer adipiscing elit. Curabitur sed',4398894895424),
+  (48,'Amery Peña','/settings','est ac mattis semper, dui lectus rutrum urna, nec luctus',3713588495868),
+  (49,'Simon Velasco','/site','id, blandit at, nisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel nisl. Quisque fringilla',9457364496724),
+  (50,'Courtney B. Campos','/en-us','dui, in sodales elit erat vitae risus. Duis a mi fringilla mi lacinia mattis. Integer',5590225037288);
---
+++ BBDD/JSOM & SQL DATA SET/Uusuairo.sql
@@ -0,0 +1,60 @@
+INSERT INTO Usuario (ID_Usuario,Nombre,Pass,Imagen,Correo,Activo,ID_Rol)
+VALUES
+  (1,'Emerson Serrano','XMO98WVV9KH','/user/110','malesuada@yahoo.ca',0,1),
+  (2,'Shay T. Peña','QXS40RIY5QY','/en-us','dignissim.tempor.arcu@aol.org',0,1),
+  (3,'Courtney H. Pardo','YMM54KRZ6ZM','/site','eleifend.nec@protonmail.edu',1,1),
+  (4,'Ivana Saez','CBW42DVK8EI','/sub/cars','nulla.integer@icloud.com',1,1),
+  (5,'Kennedy Flores','YOJ56UPU4MS','/en-ca','ornare.libero@yahoo.couk',0,1),
+  (6,'Lacey Vidal','ALV94IRP7EZ','/en-us','eget.metus@google.ca',1,1),
+  (7,'Justin U. Arias','BHF92BZK5CX','/site','in.dolor.fusce@aol.couk',0,1),
+  (8,'Mira N. Leon','KGY74UCB9XT','/sub/cars','at@google.edu',0,1),
+  (9,'Tamekah E. Gimenez','UXD75PBI6EJ','/user/110','integer.aliquam@aol.couk',0,1),
+  (10,'Drew K. Castro','XKC07ZML3QU','/one','pellentesque.eget@hotmail.edu',1,1);
+INSERT INTO Usuario (ID_Usuario,Nombre,Pass,Imagen,Correo,Activo,ID_Rol)
+VALUES
+  (11,'September K. Ramos','QZP62LXE4SL','/one','curabitur@aol.net',1,1),
+  (12,'Zeus W. Andres','NXU86XAR7SO','/one','augue.ac.ipsum@icloud.ca',1,1),
+  (13,'Risa Esteban','SIJ49UXU8BN','/en-ca','dui.nec@hotmail.net',0,1),
+  (14,'Price X. Pardo','NCT87MUD0WV','/en-us','a@icloud.org',1,1),
+  (15,'Idona S. Aguilar','RST29EZC8WO','/site','tempus.eu@outlook.edu',0,1),
+  (16,'Aidan U. Moya','QML84KDP0MG','/en-ca','magna.et@aol.org',0,1),
+  (17,'Ori J. Moreno','UTH44QMR8GO','/sub/cars','nunc.sed.pede@yahoo.com',1,1),
+  (18,'Cynthia Herrero','UKB11DUS6FI','/group/9','luctus.aliquet@icloud.net',0,1),
+  (19,'Zoe G. Marin','ZIO84YZO7CL','/en-ca','ut.pharetra@outlook.org',0,1),
+  (20,'Scarlett N. Marti','YCP96OMR3KE','/group/9','sapien@google.org',0,1);
+INSERT INTO Usuario (ID_Usuario,Nombre,Pass,Imagen,Correo,Activo,ID_Rol)
+VALUES
+  (21,'Kiona Jimenez','FML18TGP1BL','/sub/cars','et.euismod.et@yahoo.ca',1,1),
+  (22,'Remedios Alvarez','IGY46GCP5OR','/sub/cars','est.mauris@protonmail.net',0,1),
+  (23,'Laith Fuentes','NMD87JER0FT','/fr','dictum@icloud.org',0,1),
+  (24,'Brielle L. Duran','FPB99NNH5DQ','/en-us','nulla.aliquet@protonmail.edu',1,1),
+  (25,'Gary Fernandez','DCF52TUN4PC','/en-ca','augue.eu@yahoo.com',1,1),
+  (26,'Colin Moreno','KEL35GWL8HF','/group/9','libero.dui@protonmail.edu',0,1),
+  (27,'Velma Pascual','NAN38JTL1QF','/group/9','lectus.justo@outlook.couk',0,1),
+  (28,'Nero Cruz','CFK38GPT5EO','/settings','arcu.iaculis@icloud.couk',0,1),
+  (29,'Colton Gomez','LGB57GRP3RG','/fr','lorem.ipsum@yahoo.ca',1,1),
+  (30,'Cameron L. Serrano','ESB72HJD5OE','/sub','nunc@google.net',1,1);
+INSERT INTO Usuario (ID_Usuario,Nombre,Pass,Imagen,Correo,Activo,ID_Rol)
+VALUES
+  (31,'Basil Moreno','DKW58KVB1VY','/one','imperdiet.ullamcorper@protonmail.org',1,1),
+  (32,'Solomon Z. Castro','TWR21DHK1IB','/sub','sapien.aenean@yahoo.org',1,1),
+  (33,'Kenneth M. Marquez','LJY52HLT0PT','/sub/cars','magna.praesent@hotmail.org',0,1),
+  (34,'Kai S. Soto','HRI80VGS3KL','/sub/cars','aliquam@hotmail.net',1,1),
+  (35,'Alika Nuﾑez','IQS86IQK1MJ','/one','eu.odio@hotmail.couk',0,1),
+  (36,'Molly Pastor','TNW18SHY7SY','/en-ca','sed.auctor@aol.org',1,1),
+  (37,'Hannah Ferrer','WXT58NMK2RG','/site','tempor@aol.org',0,1),
+  (38,'Brady Perez','FUH17TPM1DP','/user/110','est.ac@yahoo.ca',1,1),
+  (39,'Damon Bravo','LCD70RQV5DX','/en-us','fringilla.donec@protonmail.net',0,1),
+  (40,'Erich V. Gallego','IOL15DWO6HE','/user/110','sollicitudin@google.ca',1,1);
+INSERT INTO Usuario (ID_Usuario,Nombre,Pass,Imagen,Correo,Activo,ID_Rol)
+VALUES
+  (41,'Claudia Vidal','HGO81XNH2OG','/user/110','parturient.montes@google.couk',1,1),
+  (42,'Laith F. Castro','XKC88VWT7LE','/user/110','lacus@google.org',1,1),
+  (43,'Orson Torres','CWD43CJB8UN','/group/9','feugiat.lorem@hotmail.couk',0,1),
+  (44,'Graiden Santos','RVE38PFJ5JH','/site','eu.tempor@google.org',0,1),
+  (45,'Jane Marquez','BVG20BDE7KV','/fr','egestas.duis@google.com',1,1),
+  (46,'Garrett Ibañez','JRP33TGU5DF','/user/110','risus@icloud.org',1,1),
+  (47,'Kaseem Alonso','GPJ94HFT7SZ','/en-us','massa@aol.org',0,1),
+  (48,'Dylan Hernandez','LHG27JKE3TC','/sub','nec.ante.blandit@icloud.net',0,1),
+  (49,'Megan I. Cruz','XDY96MKP6ZK','/group/9','mattis.cras.eget@icloud.edu',0,1),
+  (50,'Dacey T. ﾑez','MMH36JWN1EX','/sub','varius.et@protonmail.couk',1,1);
